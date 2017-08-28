a619080dee445e095042b4c049418ed02d06f58b
BFAlex <838318741@qq.com>
2017-08-28 09:29:54 +0800

    Initial Commit

---
+++ BFGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFGame
+//
+//  Created by Readboy_BFAlex on 2017/8/28.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFGame
+//
+//  Created by Readboy_BFAlex on 2017/8/28.
+//  Copyright © 2017年 Readboy_BFAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
